c972c47832a6c8462cba5b7705b9b1e2a43adf27
Gan Yi <ganyi521@icloud.com>
2016-07-20 17:48:13 +0100

    Add Recommend function

---
+++ app/src/main/java/com/example/cognac/test/RecommendActivity.java
@@ -0,0 +1,65 @@
+package com.example.cognac.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RecommendActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayAdapter<String>arrayAdapter;
+    private SimpleAdapter simpleAdapter;
+    private List<Map<String,Object>>dataList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend);
+
+        listView = (ListView)findViewById(R.id.moduleScroll);
+        //ArrayAdapter(上下文,当前listview加载的每一个列表项所对应的布局文件,数据源)
+        //适配器加载数据源
+        String[]arrayData = {"周杰伦","蔡依林","王力宏","林俊杰"};
+        dataList = new ArrayList<Map<String,Object>>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayData);
+        /*SimpleAdapter 参数:
+        1. context 上下文
+        2. data 数据源(List<? extends Map<String, ?>>data) 一个map所组成的List的集合
+        每一个Map都回去对应ListView列表中的一行,每一个Map(键值对)中的key,必须包含所有在from中所指定的键
+        3. resource:列表项的布局文件ID
+        4. From: Map中的键名
+        5. to 绑定数据视图中的ID
+        */
+        simpleAdapter = new SimpleAdapter(this,getData(),R.layout.item,new String[]{"pic","txt"},new int[]{R.id.pic,R.id.txt});
+        //使用视图(listview)加载适配器
+        //listView.setAdapter(arrayAdapter);
+        listView.setAdapter(simpleAdapter);
+    }
+
+    private List<Map<String,Object>> getData(){
+        for (int i=1;i<20;i++){
+
+            Map<String,Object> map = new HashMap<String, Object>();
+            map.put("pic",R.drawable.ic_local_movies_black_48dp);
+            map.put("txt","陈秋霞"+i);
+            dataList.add(map);
+
+        }
+
+        return dataList;
+    }
+
+    private void onClickRe(){
+
+
+    }
+
+
+
+
+}
